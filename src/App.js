b9f8dcbd339a29c4300f646c0d56667ed1814bb4
Ben H <deepfreespace@gmail.com>
2022-02-04 11:18:03 -0700

    Framework skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+// Imports
 import './App.css';
 
+// Component and page Imports
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Nav />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
