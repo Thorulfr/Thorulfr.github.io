db71775b3f01758e6cc2446ad3f4ab37087495c2
Ben H <deepfreespace@gmail.com>
2022-02-26 22:14:26 -0700

    Basic project card

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -3,15 +3,46 @@ import React from 'react';
 
 function Project(project) {
     return (
-        <div>
-            <p>{project.project.name}</p>
-            <p>{project.project.description}</p>
-            <p>{project.project.githubLink}</p>
-            <p>{project.project.deployedLink}</p>
-            <img
-                src={project.project.imagePath}
-                alt="Application screenshot"
-            ></img>
+        <div className="shadow-sm m-2 rounded-lg flex flex-col justify-around items-center">
+            {/* <p>{project.project.description}</p>*/}
+            <span className="text-lg text-center m-1 underline underline-offset-1 decoration-1">
+                {project.project.name}
+            </span>
+            <img src={project.project.imagePath} alt="Application screenshot" />
+            <p className="text-center">{project.project.description}</p>
+            <div className="w-1/2 flex justify-evenly items-center p-1 mb-1">
+                <a
+                    href={project.project.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        src="assets/images/github-logo.svg"
+                        className="h-8 w-8"
+                        alt="Github logo"
+                    />
+                </a>
+                <a
+                    href={project.project.deployedLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                        />
+                    </svg>
+                </a>
+            </div>
         </div>
     );
 }
