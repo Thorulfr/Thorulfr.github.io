5938ed8ab4d982e8a300e890d10164cf308d6327
Ben H <deepfreespace@gmail.com>
2022-02-27 17:02:52 -0700

    Resume embedded

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,18 @@
 import React from 'react';
 
 const Resume = () => {
-    return <main>Resume</main>;
+    return (
+        <main className="grow flex flex-col">
+            <div className="grow m-4">
+                <iframe
+                    src="assets/CV.pdf"
+                    title="Benjamin Holt's CV"
+                    height="100%"
+                    width="100%"
+                ></iframe>
+            </div>
+        </main>
+    );
 };
 
 export default Resume;
